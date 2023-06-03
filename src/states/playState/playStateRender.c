0089e0715a95fdcbff830c411d9b49963ba5fe56
pedramcode <dev.dehghanpour@gmail.com>
2023-06-03 23:11:20 +0330

    text test render

--- src/states/playState/playStateRender.c
+++ src/states/playState/playStateRender.c
@@ -2,10 +2,10 @@
 
 void playStateRender(void *state) {
     ClearBackground(RAYWHITE);
-    DrawText("Home (main state)", 20, 20, 25, DARKGRAY);
+    DrawText("Home (main state)", 20, 20, 25, BLACK);
     DrawText("This state loaded dynamically using my game engine.", 20, 80, 20, DARKGRAY);
     DrawText("I'm a proud father :)", 20, 110, 20, DARKGRAY);
     DrawText("The infrastructure is raylib.", 20, 140, 20, DARKGRAY);
     DrawText("ECS - will be the architecture.", 20, 170, 20, DARKGRAY);
-    DrawText("- Pedram", GetRenderWidth() - 200, 230, 15, DARKGRAY);
+    DrawText("- Pedram", GetRenderWidth() - 200, 230, 18, GRAY);
 }
