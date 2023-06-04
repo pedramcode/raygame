0969e9a9b9bd7b2869b416c8c76620d0deca9295
pedramcode <dev.dehghanpour@gmail.com>
2023-06-04 15:08:32 +0330

    Added Flecs

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "states/playState/playState.h"
 
 int main() {
-    App_t *app = appCreate(640, 480, "Raygame :D");
+    App_t *app = appCreate(640, 480, "Raygame");
 
     PlayState_t *playState = playStateCreate("play");
     REGISTER_STATE(app, playState);
